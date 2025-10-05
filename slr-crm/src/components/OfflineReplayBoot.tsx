8885b18717f40063d93c9c7e8f71a6680e50ec0a
Hercs1494 <121726703+Hercs1494@users.noreply.github.com>
2025-10-05 20:53:12 +0100

    Update OfflineReplayBoot.tsx

--- slr-crm/src/components/OfflineReplayBoot.tsx
+++ slr-crm/src/components/OfflineReplayBoot.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useEffect } from 'react';
-import { startOfflineReplay } from '@/lib/offline';
+import { startOfflineReplay } from '../../../lib/offline';
 
 export default function OfflineReplayBoot() {
   useEffect(() => { startOfflineReplay(); }, []);
